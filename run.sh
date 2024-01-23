c2dbe5aa4e5c3006a8f79d131e6d72d10fd714b8
Kumonda221 <kumonda@kucro3.org>
2024-01-23 23:58:15 +0800

    QuardStar Tutorial 2021 Chapter 22 (ch22)

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
 VC=\
-"1920x1080 | \
+"full-screen | \
+1920x1080 | \
 1600x900 | \
 1280x960 | \
 1280x720 | \
@@ -13,23 +14,42 @@ VC=\
 640x480 | \
 640x340"
 
+MODE=\
+"graphic | \
+nographic | \
+customize1 | \
+customize2 | \
+customize3"
+
+USAGE="usage $0 [$MODE] [$VC]"
+
 if [ $# != 1 ] ; then
 	if [ $# != 2 ] ; then
-		echo "usage $0 [graphic | nographic] [$VC]"
+		echo $USAGE
 		exit 1
 	fi
 fi
 
+if [ $# != 2 ] ; then
+	DEFAULT_VC="1280x720"
+else
+	case "$2" in
+	full-screen)
+		DEFAULT_VC="$(xrandr -q 2>/dev/zero | awk '/*/{print $1}')"
+		FULL_SCREEN=-full-screen
+		;;
+	*)
+		DEFAULT_VC=$2
+		;;
+	esac
+fi
+
+WIDTH="$(echo $DEFAULT_VC | sed 's/\(.*\)x\(.*\)/\1/g')"
+HEIGHT="$(echo $DEFAULT_VC | sed 's/\(.*\)x\(.*\)/\2/g')"
+
 case "$1" in
 graphic)
-	DEFAULT_VC=$2
-	if [ $# != 2 ] ; then
-		DEFAULT_VC="1280x720"
-	fi
-	GRAPHIC_PARAM="--display gtk,zoom-to-fit=false --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --parallel none --monitor vc:$DEFAULT_VC"
-	
-	WIDTH="$(echo $DEFAULT_VC | sed 's/\(.*\)x\(.*\)/\1/g')"
-	HEIGHT="$(echo $DEFAULT_VC | sed 's/\(.*\)x\(.*\)/\2/g')"
+	GRAPHIC_PARAM="--display gtk,zoom-to-fit=false --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --monitor vc:$DEFAULT_VC --parallel none"
 	ROWS="$(echo $WIDTH / 8 |bc)"
 	COLS="$(echo $HEIGHT / 16 |bc)"
 	DEFAULT_V=":vn:$COLS""x""$ROWS:"
@@ -39,12 +59,26 @@ nographic)
     GRAPHIC_PARAM="-nographic --parallel none"
 	DEFAULT_V=":vn:$DEFAULT_VN:"
     ;;
+customize1)
+	GRAPHIC_PARAM="--display gtk,zoom-to-fit=false --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --monitor stdio --parallel none"
+	ROWS="$(echo $WIDTH / 8 |bc)"
+	COLS="$(echo $HEIGHT / 16 |bc)"
+	DEFAULT_V=":vn:$COLS""x""$ROWS:"
+	;;
+customize2)
+	GRAPHIC_PARAM="--display gtk,zoom-to-fit=false --serial telnet::3441,server,nowait --serial telnet::3442,server,nowait --serial telnet::3443,server,nowait --monitor stdio --parallel none"
+	DEFAULT_V=":vn:24x80:"
+	;;
+customize3)
+	GRAPHIC_PARAM="--display gtk,zoom-to-fit=false --serial telnet::3441,server,nowait --serial telnet::3442,server,nowait --serial telnet::3443,server,nowait --monitor none --parallel none"
+	DEFAULT_V=":vn:24x80:"
+	;;
 --help)
-	echo "usage $0 [graphic | nographic] [$VC]"
+	echo $USAGE
 	exit 0
 	;;
 *)
-	echo "usage $0 [graphic | nographic] [$VC]"
+	echo $USAGE
 	exit 1	
 	;;
 esac
@@ -54,13 +88,13 @@ $SHELL_FOLDER/output/qemu/bin/qemu-system-riscv64 \
 -m 1G \
 -smp 8 \
 -drive if=pflash,bus=0,unit=0,format=raw,file=$SHELL_FOLDER/output/fw/fw.bin \
--drive file=$SHELL_FOLDER/output/rootfs/rootfs.img,format=raw,id=hd0 \
--global virtio-mmio.force-legacy=false \
--device virtio-blk-device,drive=hd0 \
--device virtio-gpu-device,id=video0,xres=1280,yres=720 \
--device virtio-mouse-device \
--device virtio-keyboard-device \
+-drive file=$SHELL_FOLDER/output/rootfs/rootfs.img,format=raw,id=drive0 \
 -fsdev local,security_model=mapped-xattr,id=fsdev0,path=$SHELL_FOLDER/target_root_app \
+-global virtio-mmio.force-legacy=false \
+-device virtio-blk-device,id=hd0,drive=drive0 \
+-device virtio-gpu-device,id=video0,xres=$WIDTH,yres=$HEIGHT \
+-device virtio-mouse-device,id=input0 \
+-device virtio-keyboard-device,id=input1 \
 -device virtio-9p-device,id=fs0,fsdev=fsdev0,mount_tag=hostshare \
 -fw_cfg name="opt/qemu_cmdline",string="qemu_vc="$DEFAULT_V"" \
-$GRAPHIC_PARAM
+$GRAPHIC_PARAM $FULL_SCREEN
